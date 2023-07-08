5ce23d1566b644737cd338730bf2197f8ae79b65
Sachin Kumar <sachinkumar8051798233@gmail.com>
2023-07-08 15:20:16 +0530

    initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserAuthService } from '../_service/user-auth.service';
+import { UserService } from '../_service/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+
+  constructor( private userService: UserService,
+    private userAuthService: UserAuthService,
+    private router: Router){
+
+  }
+  ngOnInit(){
+
+  }
+  login(loginForm: NgForm) {
+    this.userService.login(loginForm.value).subscribe(
+      (response: any) => {
+        this.userAuthService.setRoles(response.user.role);
+        this.userAuthService.setToken(response.jwtToken);
+
+        const role = response.user.role[0].roleName;
+        if (role === 'Admin') {
+          this.router.navigate(['/admin']);
+        } else {
+          this.router.navigate(['/user']);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
